3e142cdda26164a31b46ad04a5a40727f869a36e
Shruti <shruti.dsms@gmail.com>
2022-11-22 20:15:34 +0530

    first

---
+++ src/mockData.js
@@ -0,0 +1,74 @@
+export const contactList = [
+  {
+    id: 1,
+    name: "Shruti",
+    profilePic: "/profile/Shruti.png",
+    lastText: "Hey Man",
+    lastTextTime: "12:58 PM",
+  },
+  {
+    id: 2,
+    name: "Ronaldo",
+    profilePic: "/profile/Ronaldo.jfif",
+    lastText: `I am the G.O.A.T.`,
+    lastTextTime: "12:45 PM",
+  },
+  {
+    id: 3,
+    name: "Lionel Messi",
+    profilePic: "/profile/messi.jfif",
+    lastText: "I am loving PSG",
+    lastTextTime: "12:30 PM",
+  },
+  {
+    id: 4,
+    name: "Neymar",
+    profilePic: "/profile/neymar.jpg",
+    lastText: "Call Mbappe?",
+    lastTextTime: "12:00 PM",
+  },
+];
+export const messagesList = [
+  {
+    id: 1,
+    messageType: "TEXT",
+    text: "Hey Man 😁",
+    senderID: 0,
+    addedOn: "12:00 PM",
+  },
+  {
+    id: 2,
+    messageType: "TEXT",
+    text: "Hey, What's up?",
+    senderID: 1,
+    addedOn: "12:01 PM",
+  },
+  {
+    id: 3,
+    messageType: "TEXT",
+    text: "All Good, What about you?",
+    senderID: 0,
+    addedOn: "12:00 PM",
+  },
+  {
+    id: 4,
+    messageType: "TEXT",
+    text: "I'm good as well",
+    senderID: 1,
+    addedOn: "12:00 PM",
+  },
+  {
+    id: 5,
+    messageType: "TEXT",
+    text: "Great 😁",
+    senderID: 0,
+    addedOn: "12:00 PM",
+  },
+  {
+    id: 5,
+    messageType: "TEXT",
+    text: "Did you watch UCL final ?",
+    senderID: 1,
+    addedOn: "12:00 PM",
+  },
+];
